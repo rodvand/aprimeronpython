2a19043060a9525a34b5311e838e02a114ec9687
Martin Rødvand <martin@rodvand.net>
2010-12-16 03:39:31 +0100

    1.10 done.

---
+++ chapter1/Gaussian_function1.py
@@ -0,0 +1,20 @@
+import sys
+from math import pi, exp, sqrt
+
+m = 0
+s = 2
+x = 1
+
+if s <= 0:
+    print 's must be greater than zero!'
+    sys.exit(1)
+
+f = (1./(sqrt(2 * pi) * s)) * exp(-0.5 * ((x -m)/float(s))**2)
+
+print f
+
+"""
+Running the program
+Unix>python Gaussian_function1.py 
+0.176032663382
+"""
