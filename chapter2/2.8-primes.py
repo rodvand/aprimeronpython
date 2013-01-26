20e338cf4a1b77f662702cab666c6e4259d5f1ce
Martin Rødvand <martin@rodvand.net>
2013-01-26 01:52:50 +0100

    Added 2.8

---
+++ chapter2/2.8-primes.py
@@ -0,0 +1,21 @@
+primes = [1, 3, 5, 7, 11, 13]
+
+for number in primes:
+    print number
+
+p = 17
+primes.append(p)
+
+print primes
+
+"""
+Running program
+Unix>python 2.8-primes.py
+1
+3
+5
+7
+11
+13
+[1, 3, 5, 7, 11, 13, 17]
+"""
